13645d209bb0c0572abf509d812fe61c38be2bc4
MasonA <mason@ourprairie.net>
2018-10-01 14:05:23 -0700

    Add challenges

---
+++ javascript/practice_problems/dom-search.js
@@ -0,0 +1,5 @@
+// Write a bfs style method which traverses the dom and returns an array of elements which have a "src" attribute
+// How would you do this using just vanilla dom manipulation?
+
+// Write a dfs style method which traverses the dom and returns an array of elements which have a "src" attribute
+// How would you do this using just vanilla dom manipulation?
